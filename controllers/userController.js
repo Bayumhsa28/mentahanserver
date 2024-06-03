d66901b4ae6cf2cd0d44aead38d7dc04ebe4dfba
TheGodOfBay <bayumhsa128@gmail.com>
2024-06-04 02:21:05 +0700

    telah langsung ke database xampp

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,38 +1,107 @@
 const database = require("../database.js");
 const getAllUsers = (req, res) => {
     database.query(`SELECT * FROM USERS`, (err, result) => {
-        if(err){
-            res.status(500).json({err: "Something wrong"})
+        if (err) {
+            res.status(500).json({ err: "Something wrong" })
             throw err
         }
         console.log(result);
-        res.json({result});
-    })
+        res.json({ result });
+    });
 };
 const getUserById = (req, res) => {
     const { id } = req.params;
-    if(!id){
+    if (!id) {
         return res.status(400).json({
             error: "silahkan isi field id users!",
         });
     }
-    res.json({
-        data: {
-            name: "pras",
-            email: "pras@gmail.com",
-            password: "pras1122",
+    database.query(`SELECT * FROM users WHERE id = ?`, [id], (err, results) => {
+        if (err) {
+            console.error(err)
+            return res.status(500).json({
+                error: "Error while getting user with ID"
+            })
         }
+        if (results.length === 0) {
+            return res.json({
+                message: "User not found",
+                results: []
+            })
+        }
+        res.json({ results });
     });
 };
-const updateUserById = (req, res) => {
 
+const createNewUser = (req, res) => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+        return res.status(400).json({
+            error: "Silahkan isi field name, email, dan password!",
+        });
+    }
+    database.query(`INSERT INTO users(name, email, password) VALUES (?, ?, ?)`,
+        [name, email, password],
+        (err, results) =>  {
+            if(err){
+                console.error(err)
+                return res.status(500).json({
+                    error: "Error while inserting new user!"
+                })
+            }
+            if(results.affectedRows > 0){
+                console.log(results)
+                return res.json({
+                    message: "New Users Create"
+                })
+            }
+            return res.status(500).json({
+                error: "No user created!"
+            })
+        }
+    );
+}
+const updateUserById = (req, res) => {
+    const { id } = req.params;
+    const { name, email, password } = req.body;
+    if(!id || !name || !email || !password){
+        return res.status(400).json({
+            error: "Silahkan isi field id, name, email, dan password!",
+        });
+    }
+    console.log(id);
+    database.query(`UPDATE users set name = ?, email = ?, password = ?
+    WHERE id = ?`, [name, email, password, id], (err, results) => {
+        if(err){
+            console.error(err);
+            res.status(500).json({
+                error: "Error While Updating user!",
+            });
+        }
+        if(results.affectedRows === 0){
+            return res.status(400).json({
+                error: "User dengan ID " + id + " tidak ditemukan, gagal update"
+            });
+        }
+        return res.json({
+            users: results,
+            message: "Data user dengan ID " + id + " telah diubah"
+        })
+    })
 };
 const deleteUserById = (req, res) => {
-
+    const { id } = req.params;
+    if (!id) {
+        return res.status(400).json({
+            error: "silahkan isi field id users!",
+        });
+    }
+    res.json({ message: `user dengan ID ${id} telah dihapus!` });
 };
 module.exports = {
-    getAllUsers, 
-    getUserById, 
-    updateUserById, 
+    getAllUsers,
+    getUserById,
+    updateUserById,
     deleteUserById,
+    createNewUser,
 };
--- server.js
+++ server.js
@@ -22,48 +22,12 @@ app.get("/", (req, res) => {
 app.get("/API/users", userController.getAllUsers);
 
 //method post menambahkan data user baru
-app.post("/api/users", (req, res) => {
-    const { name, email, password } = req.body;
-    if(!name || !email || !password){
-        return res.status(400).json({
-            error: "Silahkan isi field name, email, dan password!",
-        });
-    }
-    res.json({
-        name: name,
-        email: email,
-        password: password,
-        message: "User telah ditambahkan",
-    });
-});
+app.post("/api/users", userController.createNewUser);
 
 //metode put
-app.put("/api/users/:id", (req, res) => {
-    const { id } = req.params;
-    const { name, email, password } = req.body;
-    if(!id || !name || !email || !password){
-        return res.status(400).json({
-            error: "Silahkan isi field id, name, email, dan password!",
-        });
-    }
-    console.log(id);
-    res.json({ 
-        message: "data users dengan id" + id + "telah diubah",
-        name: name,
-        email: email,
-        password: password,
-     });
-});
+app.put("/api/users/:id", userController.updateUserById);
 
-app.delete("/api/users/:id", (req, res) => {
-    const { id } = req.params;
-    if(!id){
-        return res.status(400).json({
-            error: "silahkan isi field id users!",
-        });
-    }
-    res.json({message: `user dengan ID ${id} telah dihapus!`});
-});
+app.delete("/api/users/:id", userController.deleteUserById);
 
 app.get("/api/users/:id", userController.getUserById);
 
