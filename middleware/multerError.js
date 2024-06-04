0d765165fc51c3e2c84ad14dd3ae3781c3446a4a
TheGodOfBay <bayumhsa128@gmail.com>
2024-06-04 10:07:21 +0700

    lanjut

---
+++ middleware/multerError.js
@@ -0,0 +1,12 @@
+const multer = require('multer');
+const multerError = (err, req, res, next) => {
+    if(err instanceof multer, multer.MulterError){
+        if(err.code === "LIMIT_FILE_SIZE"){
+            return res.status(400).json({
+                error: "File size too large"
+            });
+        }
+        return res.status(400).json({error: err.message});
+    }
+};
+module.exports = multerError;
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const app = express();
-const database = require("./database");
 const userController = require("./controllers/userController.js")
+const errorhandler = require("./middleware/errorHandler.js");
+const database = require("./database");
+const logger = require("./middleware/logger.js");
+const upload = require("./utils/upload.js");
+const multerError = require("./middleware/multerError.js");
+
+
+const app = express();
 const PORT = 8000;
 
 //middleware
@@ -9,6 +15,8 @@ const PORT = 8000;
 app.use(express.json());
 //menangani data dari client atau browser
 app.use(express.urlencoded({extended: true}));
+//menangani error 
+app.use(errorhandler);
 
 
 //route http://localhost:8000
@@ -30,6 +38,10 @@ app.put("/api/users/:id", userController.updateUserById);
 app.delete("/api/users/:id", userController.deleteUserById);
 
 app.get("/api/users/:id", userController.getUserById);
+
+app.post("/file-upload", upload.single("file"), multerError, (req, res) => {
+    res.json({ message: "File uploaded!" });
+});
 
 app.listen(PORT, () =>
     console.log(`Server is running on http://localhost:${PORT}`)
